e77464ca88d091befb5b56838405f160075e6fb0
nightfarers-359 <abismo359@qq.com>
2026-04-19 10:51:19 +0800

    基础框架搭建

---
+++ src/main/java/com/platform/services/auth.java
@@ -0,0 +1,9 @@
+package com.platform.services;
+
+import com.platform.uitls.BaseClass.LoginData;
+public class auth {
+    //用户登录
+    public void login(LoginData loginData) {
+        System.out.println("login service");
+    }
+}
