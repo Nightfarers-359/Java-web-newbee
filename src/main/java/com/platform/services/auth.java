5d7fc777a4ead5d8c07da3d4378fb5054c2f3972
Nightfarers-359 <abismo359@qq.com>
2026-04-20 15:59:28 +0800

    Delete src directory

--- src/main/java/com/platform/services/auth.java
+++
@@ -1,9 +0,0 @@
-package com.platform.services;
-
-import com.platform.uitls.BaseClass.LoginData;
-public class auth {
-    //用户登录
-    public void login(LoginData loginData) {
-        System.out.println("login service");
-    }
-}
