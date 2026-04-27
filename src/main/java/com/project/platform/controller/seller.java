442e5850317940bf0095355cbac94ad94b8e2d7f
Zyduannnn <18161200383@163.com>
2026-04-28 01:08:01 +0800

    以解决Orders.java与Users.java合并冲突

--- src/main/java/com/project/platform/controller/seller.java
+++ src/main/java/com/project/platform/controller/seller.java
@@ -1,37 +1,12 @@
 package com.project.platform.controller;
 
-import com.project.platform.entity.Comments;
-import com.project.platform.entity.Orders;
-import com.project.platform.service.SellerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
-@RequestMapping("/seller/orderlist")
-public class seller {
-    @Autowired
-    SellerService sellerService;
+@RequestMapping("/seller")
+public class Seller {
 
-    @PostMapping
-    public void addOrderList(@RequestBody Orders order){
-        sellerService.addOrderList(order);
-    }
-    @PostMapping
-    public void cancelOrderList(@RequestBody Orders order){
-        sellerService.cancelOrderList(order);
-    }
-    @GetMapping("/{id}")
-    public Orders getOrderListByid(@PathVariable Integer id){
-        return sellerService.getOrderListByid(id);
-    }
-    @GetMapping("/{userid}")
-    public List<Orders> getOrderListByids(@PathVariable Long userid){
-        return sellerService.getOrderListByids(userid);
-    }
-    @PostMapping
-    public void commentItem(@RequestBody Comments comment){
-        sellerService.commentItem(comment);
-    }
+    
 }
